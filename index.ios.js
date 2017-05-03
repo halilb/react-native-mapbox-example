f501fc7eeb0a43c3614f81cd57e1d6f1ccc625d3
Halil Bilir <hbilir@gmail.com>
2017-05-03 13:20:37 +0300

    STEP-4 rendering a basic MapBox

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import Mapbox, { MapView } from 'react-native-mapbox-gl';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  View,
 } from 'react-native';
 
-export default class MapboxExample extends Component {
+const accessToken = 'your-token-here';
+Mapbox.setAccessToken(accessToken);
+
+class MapboxExample extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <MapView
+          style={styles.map}
+          styleURL={Mapbox.mapStyles.dark}
+          showsUserLocation
+        />
       </View>
     );
   }
@@ -34,19 +26,10 @@ export default class MapboxExample extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'stretch',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  map: {
+    flex: 1,
   },
 });
 
